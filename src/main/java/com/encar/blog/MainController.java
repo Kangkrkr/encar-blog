7394cc24c63955aa908434e75b2fee2b96428945
SKencarn-PC\SKencarn <kangkrkr@naver.com>
2017-01-20 18:57:58 +0900

    init

--- src/main/java/com/encar/blog/MainController.java
+++ src/main/java/com/encar/blog/MainController.java
@@ -2,30 +2,18 @@ package com.encar.blog;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.encar.blog.domain.Member;
-import com.encar.blog.domain.MemberAuth;
-import com.encar.blog.service.MemberService;
 
 @Controller
 public class MainController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 	
-	@Autowired
-	private MemberService memberService;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Model model) {
-		
-		Member member = memberService.selectMemberWithAccountAndAuthName(new MemberAuth("kangkrkr", "ADMIN"));
-		model.addAttribute("member", member);
-		
+	public String home() {
 		return "home";
 	}
 	
@@ -34,8 +22,8 @@ public class MainController {
 		return "login/login";
 	}
 	
-	@RequestMapping(value = "/login2", method = RequestMethod.GET)
+	@RequestMapping(value = "/posting", method = RequestMethod.GET)
 	public String login2() {
-		return "login/login2";
+		return "posting/posting";
 	}
 }
